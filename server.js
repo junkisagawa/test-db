b2f2f330fc96426cce52d5ea1785a29ad765d838
Junki Sagawa <aha04612@jp.ibm.com>
2022-07-06 13:31:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
 });
 
 // 追加
-app.use(express.static(__dirname + '/dist/'))
+app.use(express.static(__dirname + '/distStoreFrontHouse'))
 
 
 const { WatsonMLScoringEndpoint } = require("watson-ml-model-utils");
